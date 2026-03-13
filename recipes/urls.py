d1fe0daa230ea80a71b165a841001fe79cfd0a40
Claudia Yalitza Inzunza Escarcega <L20170702@culiacan.tecnm.mx>
2026-03-12 17:29:04 -0700

    Page my recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import RecipeCreateView, RecipeDetailView, toggle_favorite, RecipeUpdateView
+from .views import (
+    RecipeCreateView, RecipeDetailView, 
+    toggle_favorite, RecipeUpdateView, 
+    RecipeDeleteView, MyRecipesView)
 
 
 urlpatterns = [
     path('add-recipe', RecipeCreateView.as_view(), name="recipe_create"),
     path('detail/<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
     path('recipe/<int:pk>/favorite/', toggle_favorite, name='toggle_favorite'),
-    path('update-recipe/<int:pk>', RecipeUpdateView.as_view(), name="update_recipe")
+    path('update-recipe/<int:pk>', RecipeUpdateView.as_view(), name="update_recipe"),
+    path('delete-recipe/<int:pk>', RecipeDeleteView.as_view(), name="delete_recipe"),
+    path('my-recipes', MyRecipesView.as_view(), name="my_recipes")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse, reverse_lazy
 from .models import Recipe
 from .forms import RecipeForm, IngredientFormSet
 
@@ -55,11 +55,67 @@ def toggle_favorite(request, pk):
     return redirect('recipe_detail', pk=pk)
 
 
-class RecipeUpdateView(UpdateView):
+class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Recipe
     fields = ['title', 'category', 'short_description', 'cooking_time', 'instructions']
     template_name = 'recipes/update_recipe.html'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        if self.request.POST:
+            context['ingredients'] = IngredientFormSet(self.request.POST, instance=self.object)
+        else:
+            context['ingredients'] = IngredientFormSet(instance=self.object)
+        return context
+    
+    def form_valid(self, form):
+        context = self.get_context_data()
+        ingredients = context['ingredients']
+        
+        if form.is_valid() and ingredients.is_valid():
+            self.object = form.save()
+            ingredients.instance = self.object
+            ingredients.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+    
     def test_func(self):
         recipe = self.get_object()
         return recipe.author == self.request.user
+    
+    
+class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Recipe
+    template_name = 'recipes/delete_recipe.html'
+    success_url = reverse_lazy("my_recipes")
+    
+    def test_func(self):
+        recipe = self.get_object()
+        return recipe.author == self.request.user
+    
+    
+class MyRecipesView(LoginRequiredMixin, ListView):
+    model = Recipe
+    template_name = 'recipes/my_recipes.html'
+    context_object_name = "recipes"
+    paginate_by = 6
+    ordering = ['-date_of_post']
+    
+    def get_queryset(self):
+        return Recipe.objects.filter(author=self.request.user).order_by('-date_of_post')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        user_recipes =self.get_queryset()
+        context['total_recipes'] = user_recipes.count()
+        
+        total_favorites = 0
+        for recipe in user_recipes:
+            total_favorites += recipe.favorites.count()
+            
+        context['total_favorites'] = total_favorites
+        
+        return context
